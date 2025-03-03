9792a2ad331312cb487bc974ccb6dd059651f168
Amardeep Lakshkar <info.amardeeppvt@gmail.com>
2025-03-03 18:57:34 +0530

    Beta v2

--- components/providers/LoaderProvider.tsx
+++ components/providers/LoaderProvider.tsx
@@ -3,20 +3,23 @@ import React from 'react'
 import Loader from '../Loader'
 import { useTelegram } from './TelegramData'
 
-const LoaderProvider = ({children}:{children:React.ReactNode}) => {
-    const {userData} = useTelegram()
-  return (
-    <>
-     {
-                    userData ?
-                        <>
-                            {children}
-                        </>
-                        :
-                        <Loader />
-                }
-    </>
-  )
+const LoaderProvider = ({ children }: { children: React.ReactNode }) => {
+    const { loading, userData, error } = useTelegram()
+
+    if (loading) {
+        return <Loader />
+    }
+    if (!userData) {
+        return <div>Open in Telegram</div>
+    }
+    if (error) {
+        return <div>{error}</div>
+    }
+    return (
+        <>
+            {children}
+        </>
+    )
 }
 
 export default LoaderProvider
