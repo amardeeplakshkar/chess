ee78fcf2d71c877360eacb92433ac8c5dcbff74d
Amardeep Lakshkar <109571797+amardeeplakshkar@users.noreply.github.com>
2025-04-17 05:37:16 +0530

    Create ModalProvider.tsx

---
+++ components/providers/ModalProvider.tsx
@@ -0,0 +1,23 @@
+import { checkpoints} from '@/constants
+import { useUser } from '@/components/providers/UserProvider'
+import CheckInBox from '@/components/CheckInBox'
+const ModalProvider = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useUser()
+
+  return (
+    <>
+      {checkpoints.map((data, i) => (
+        <CheckInBox
+          key={i}
+          day={data.day}
+          points={data.number}
+          userId={user?.telegramId}
+          checkpointId={data.id}
+        />
+      ))}
+      {children}
+    </>
+  )
+}
+
+export default ModalProvider
